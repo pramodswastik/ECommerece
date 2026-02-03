f6d5e0bdfb54c7d5e91d793e14cd5e14eef2608e
Siddhant Mishra <mishrasiddhant4321@gmail.com>
2026-02-03 19:53:48 +0530

    check 2

---
+++ okji.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function Counter() {
+  // component-level state
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
